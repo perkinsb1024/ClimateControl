679f4971726b056511deb006191985d3eeeecaab
Ben Perkins <perkinsb1024@gmail.com>
2017-12-28 11:18:51 -0500

    Initial Commit

---
+++ ClimateControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ClimateControl
+//
+//  Created by Ben Perkins on 12/28/17.
+//  Copyright © 2017 Ben Perkins. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ClimateControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClimateControl
+//
+//  Created by Ben Perkins on 12/28/17.
+//  Copyright © 2017 Ben Perkins. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
